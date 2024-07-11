0503d118d4f9850ebdcf17c099a663915ffd1220
gabrielddrs <gabrieldavid.alu.lmb@gmail.com>
2024-07-11 14:30:34 -0300

    feat: User Module error fix

--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from 'src/presentation/user/user.controller';
 import { UserService } from './service/service.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([UserEntity])],
     controllers: [UserController],
     providers: [UserService],
+    exports: [UserService]
 })
 
 export class UserModule {}
